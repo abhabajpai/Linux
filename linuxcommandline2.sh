ae3080f91724f46f3d84c54d8b27a65e1a48e445
abhabajpai <105857736+abhabajpai@users.noreply.github.com>
2023-06-24 15:32:08 +0530

    Add files via upload

---
+++ linuxcommandline2.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+ touch abha2.txt
+cat > abha2.txt
+rmdir -p a/b/c/d
+mkdir -p name/mark
+touch abha3.txt
+cp -R dir1 dir2
+mv abha3.txt abha2.txt
+nano file
+pidof Desktop
+kill pid
+echo hello world
+chmod 0+w abha2.txt
+chmod ug=rw abha2.txt
